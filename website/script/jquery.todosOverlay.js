dc2d10619ae37dc2c1498fe67604e5a42f0a7029
lrbabe <lrbabe@lrbabe-laptop>
2009-07-10 03:20:18 +0200

    - added alternative mockups for embedded player - updated todosOverlay to display the title of the image

--- website/script/jquery.todosOverlay.js
+++ website/script/jquery.todosOverlay.js
@@ -6,6 +6,7 @@ $("body").append((
 			"<img id=\"overlayClose\" src=\"image/close.png\" />"+
 			"<img id=\"overlayLoader\" src=\"image/loader.gif\" />"+
 			"<img id=\"overlayImage\" />"+
+			"<p id=\"overlayTitle\"></p>"+
 		"</div>"+
 	"</div>"+
 	"<div id=\"fixedDiv\"></div>"
@@ -13,7 +14,7 @@ $("body").append((
 var $overlay = $("#overlayDiv").bind("click close", function() {
 		// Cache useful values
 		var overlayWidth = $overlay.data("width");
-		$overlay.animate({
+		$overlay.removeClass("maximized").animate({
 			width: 0,
 			height: 0,
 			left: "+=" + overlayWidth /2,
@@ -25,12 +26,19 @@ var $overlay = $("#overlayDiv").bind("click close", function() {
 			}).attr("src", "");
 			$overlay.dequeue();
 		});		
+	}).hover(function() {
+		if($overlay.hasClass("maximized"))
+			$title.fadeIn();
+		console.log($overlay.hasClass("maximized"))
+	}, function() {
+		$title.fadeOut();
 	}),
 	$image = $("#overlayImage").load(function() {
 		$image.css({
 			width: "90%"
 		}).animate({opacity: 1});		
 	}),
+	$title = $("#overlayTitle").css("opacity", .8),
 	$fixed = $("#fixedDiv"),
 	Format;
 	
@@ -53,7 +61,10 @@ $.fn.extend({
 							.css({
 								opacity: 0,
 								display: "block"
-							});
+							}),
+						title = $target.attr("title");
+					$title.text(title);
+					$title.css("visibility", title? "visible" : "hidden");
 					Format = targetWidth / targetHeight;
 					$overlay.data("width", options.width)
 					// Move the overlay over the clicked image
@@ -69,6 +80,9 @@ $.fn.extend({
 						left: fixedPosition.left - 300,
 						width: overlayWidth,
 						height: overlayWidth / Format
+					}).queue(function() {
+						$title.fadeIn();
+						$overlay.addClass("maximized").dequeue();
 					});
 				}
 			});
